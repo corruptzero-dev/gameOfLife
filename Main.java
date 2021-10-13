37e6735f9e8424930fe082ab2af7180798168ac1
corruptzero-dev <74492746+corruptzero-dev@users.noreply.github.com>
2021-10-13 19:56:02 +0300

    better version

--- Main.java
+++ Main.java
@@ -7,29 +7,29 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
 
-    private static final int SIZE = 20; // оптимально: 20
+    private static final int SIZE = 25;
     private static final String LIVE = "⬛";
     private static final String DEAD = "◻";
-    private static final int DELAY = 250; // оптимально: 500
-    private static final int GENERATIONS = 100; 
+    private static final int DELAY = 250;
 
     public static void main(String[] args) throws InterruptedException {
-        int[][] field = new int[SIZE][SIZE];
+        boolean[][] field = new boolean[SIZE][SIZE];
         Random random = new Random();
         for (int i=0; i<field.length; i++) {
             for (int j=0; j<field[i].length; j++) {
                 boolean ceil = random.nextBoolean();
-                field[i][j]=ceil?1:0;
+                field[i][j]=ceil;
             }
         }
-        int[][] buffer;
-        int[][] buffer2 = new int[SIZE][SIZE];
+        boolean[][] buffer;
+        boolean[][] buffer2 = new boolean[SIZE][SIZE];
         System.out.println();
 
         boolean isStopped = false;
         boolean isLooped = false;
         boolean isDead = false;
-        for (int i = 0; i <= GENERATIONS; i++) {
+        short i = 0;
+        while (!isLooped && !isDead && !isStopped){
             buffer = field;
             if (i % 2 == 0) buffer2 = field;
             field = nextGen(field);
@@ -45,7 +45,7 @@ public class Main {
                 isLooped = true;
                 break;
             }
-            if (Arrays.deepEquals(field,new int[SIZE][SIZE])){
+            if (Arrays.deepEquals(field,new boolean[SIZE][SIZE])){
                 System.out.println("Игра окончена (Смерть клеток). \nПоследнее поколение");
                 showField(field);
                 isDead = true;
@@ -55,36 +55,33 @@ public class Main {
             showField(field);
             TimeUnit.MILLISECONDS.sleep(DELAY);
             System.out.println(System.lineSeparator().repeat(50));
-        }
-        if (!isStopped && !isLooped && !isDead){
-            System.out.println("Игра окончена. Последнее поколение");
-            showField(field);
+            i++;
         }
 
     }
-    static int[][] nextGen(int[][] field) {
-        int[][] nextField = new int[SIZE][SIZE];
+    static boolean[][] nextGen(boolean[][] field) {
+        boolean[][] nextField = new boolean[SIZE][SIZE];
         for (int l = 1; l < SIZE - 1; l++) {
             for (int m = 1; m < SIZE - 1; m++) {
                 int alive = 0;
                 for (int i = -1; i <= 1; i++) {
                     for (int j = -1; j <= 1; j++) {
-                        alive += field[l + i][m + j];
+                        alive += (field[l + i][m + j]?1:0);
                     }
                 }
-                alive -= field[l][m];
-                if ((field[l][m] == 1) && (alive < 2)) nextField[l][m] = 0;
-                else if ((field[l][m] == 1) && (alive > 3)) nextField[l][m] = 0;
-                else if ((field[l][m] == 0) && (alive == 3)) nextField[l][m] = 1;
+                alive -= (field[l][m]?1:0);
+                if ((field[l][m]) && (alive < 2)) nextField[l][m] = false;
+                else if ((field[l][m]) && (alive > 3)) nextField[l][m] = false;
+                else if ((!field[l][m]) && (alive == 3)) nextField[l][m] = true;
                 else nextField[l][m] = field[l][m];
             }
         }
         return nextField;
     }
-    public static void showField(int[][] field){
-        for (int[] x : field){
-            for (int y : x){
-                System.out.print(y==1?LIVE:DEAD);
+    public static void showField(boolean[][] field){
+        for (boolean[] x : field){
+            for (boolean y : x){
+                System.out.print(y?LIVE:DEAD);
             }
             System.out.println();
         }
